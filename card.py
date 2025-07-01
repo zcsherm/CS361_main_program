d23b637dd665277667d69808487f95c18e675575
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-07-01 12:35:00 -0700

    Create card.py

---
+++ card.py
@@ -0,0 +1,39 @@
+class PlayerCard:
+    def __init__(data, import=None):
+        self._cost = data.cost
+        self._revenue = data.revenue
+        self._team = data.team
+        self._name = data.name
+        self._speed = data.speed
+        self._pinch = data.pinch_hit
+        self._deck = data.deck
+        self._hits = data.hits
+        self._action = data.action
+        pass
+
+    def get_cost(self):
+        pass
+
+    def get_revenue(self):
+        pass
+
+    def get_team(self):
+        pass
+
+    def get_name(self):
+        pass
+
+    def get_speed(self):
+        pass
+
+    def get_pinch_hitter(self):
+        pass
+
+    def get_deck(self):
+        pass
+
+    def get_hits(self):
+        pass
+
+    def get_action(self):
+        pass
