a90b476db192151b188f57b8310546418c779797
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-07-28 11:09:23 -0700

    Update new_user_screen.py

--- new_user_screen.py
+++ new_user_screen.py
@@ -6,17 +6,20 @@ import Pmw
 # Add calls to launch player screen and season screen
 
 INSTRUCTIONS = "This process will walk you through setting up a new user, new season, and a new team."
-class AddUserScreen(tk.Toplevel):
-    def __init__(self,master=None, data=None):
 
-        #self._window = tk.Tk()
-        #self._window.config(width=600, height=600)
-        #self._window.title("New User")
+class AddUserScreen(tk.Toplevel):
+    """
+    Screen that creates a new user of the program
+    """
+    def __init__(self,master=None, data=None):
+        # Setup attributes
         super().__init__()
         self._window = self
         self.title("New User")
         self._master=master
-        Pmw.initialise(self._master.root)
+        Pmw.initialise(self._master.root) # Allows for hover text
+
+        # Setup frames
         frame1 = tk.LabelFrame(self._window,borderwidth=0) # For the title
         frame1.grid(row=0,column=0,columnspan=3)
         frame2 = tk.LabelFrame(self._window, borderwidth=0) # For enter name
@@ -27,16 +30,24 @@ class AddUserScreen(tk.Toplevel):
         frame4.grid(row=1, column=2, rowspan=1)
         frame5 = tk.LabelFrame(self._window,borderwidth=0) # For buttons
         frame5.grid(row=2, column=1)
+        
+        # Setup the Title 
         txt1 = "Please enter your name:"
         label = tk.Label(frame1, text=txt1)
         label.pack(side='top',  fill='both',  padx=10,  pady=5,expand=False)
+
+        # Setup instructions
         label = tk.Label(frame4, text = INSTRUCTIONS, wraplength=100)
         label.pack(side=tk.TOP,  fill='both',  padx=10,  pady=5,  expand=True)
+
+        # Setup Name entry
         label = tk.Label(frame2, text='Name:')
         label.pack(anchor='n',padx=10,pady=5,expand=False, fill='none')
         self._name_var = tk.StringVar()
         self._entry = tk.Entry(frame3,width = 20,textvariable=self._name_var)
         self._entry.pack(side='top',padx=10,pady=5)
+
+        # Set up add and return buttons and bind hot keys
         self._add_user_button = tk.Button(frame5, text="Add", command=self.add_user, underline =0)
         self._add_user_button.pack(side='top',  fill='both',  padx=10,  pady=10,  expand=True)
         self._return_button = tk.Button(frame5, text="Return", command=self.go_to_main, underline=0)
@@ -46,7 +57,11 @@ class AddUserScreen(tk.Toplevel):
         self.focus_set()
 
     def add_user(self, event=None):
-        #response = messagebox.askyesno("Add season", "Would you like to create a starting season?")
+        """
+        Adds the entered user, also asks if the user would like to create an initial season
+        """
+
+        # Sets up a question dialog box with hover text
         self.response = Pmw.MessageDialog(self._master.root,
                                         title="Create Season?",
                                         message_text="Would you like to create a starting season?.",
@@ -57,6 +72,9 @@ class AddUserScreen(tk.Toplevel):
         hover.bind(self.response.interior(), 'A season lets you record progress over a large series of games')
 
     def go_to_main(self,event=None):
+        """
+        Returns to the login screen
+        """
         response = messagebox.askyesno("Return to main menu?", "This will return you to the home screen, any unsaved changes will be lost. Are you sure?")
         if response:
             self._master.launch_login(self)
@@ -64,6 +82,9 @@ class AddUserScreen(tk.Toplevel):
             return
 
     def season_add(self, selected):
+        """
+        Saves the new user and launches the appropriate screen 
+        """
         if selected == 'Add Season':
             self.response.withdraw()
             name = self._entry.get()
